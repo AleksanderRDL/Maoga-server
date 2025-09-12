0f1b32307634ec2a0fe5f7ed61f8741ef24929d1
AleksanderRDL <55142567+AleksanderRDL@users.noreply.github.com>
2025-09-12 14:27:52 +0200

    new mongo test version

--- test/setup.js
+++ test/setup.js
@@ -18,7 +18,7 @@ before(async function () {
 
     try {
         mongoServer = await MongoMemoryServer.create({
-            binary: { version: process.env.MONGOMS_VERSION || '6.0.6' }
+            binary: { version: process.env.MONGOMS_VERSION || '8.0' }
         });
         process.env.MONGODB_URI = mongoServer.getUri();
 
